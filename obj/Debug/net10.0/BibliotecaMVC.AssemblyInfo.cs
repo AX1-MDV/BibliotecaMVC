1596fdfbc34a60da2270489565b7f388839e245d
ALDO XAVIER LANDAVERDE MIRANDA <2025010874@cvirtualuees.edu.sv>
2026-08-13 20:20:18 -0600

    Agregado css y bootstrap en home y about.

--- obj/Debug/net10.0/BibliotecaMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/BibliotecaMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6cb92fea5542981fbf5624e4e069e22eafce43d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507aa36631c5ab76ac3f64dadcaf95febe1bb57c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
